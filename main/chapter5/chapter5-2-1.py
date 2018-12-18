0f8ac05ca8cc5ca1cb9d2fbc119441db1c125879
zhangzhichen <2788375281@qq.com>
2018-12-18 21:54:50 +0800

    提交完整训练手写数字数据集

--- main/chapter5/chapter5-2-1.py
+++ main/chapter5/chapter5-2-1.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import datetime
 from tensorflow.examples.tutorials.mnist import input_data
 
 #MNIST数据集相关的常数。
@@ -13,7 +14,7 @@ BATCH_SIZE = 100 #一个训练batch中训练数据个数，数字越小，越接
 LEARNING_RATE_BASE = 0.8  #基础学习率
 LEARNING_RATE_DECAY = 0.99 #学习率的衰减率
 REGULARIZATION_RATE = 0.0001 #描述模型复杂度的正则化损失项在损失函数中的系数。
-TRAINGING_STEPS = 30000   #训练轮数。
+TRAINGING_STEPS = 3000   #训练轮数。
 MOVING_AVERAGE_DECAY = 0.99 #滑动平均衰减率
 
 def inference(input_tensor,avg_class,weights1,biases1,weights2,biases2):
@@ -34,7 +35,7 @@ def train(mnist):
     y_ = tf.placeholder(tf.float32,[None,OUTPUT_NODE],name='y-input')
 
     #生成隐藏层的参数
-    weights1 = tf.Variable(tf.trainable_variables([INPUT_NODE,LAYER1_NODE],stddev=0.1))
+    weights1 = tf.Variable(tf.truncated_normal([INPUT_NODE,LAYER1_NODE],stddev=0.1))
     biases1 = tf.Variable(tf.constant(0.1,shape=[LAYER1_NODE]))
 
     #生成输出层的参数
@@ -71,3 +72,50 @@ def train(mnist):
     #设置指数衰减的学习率
     learning_rate = tf.train.exponential_decay(LEARNING_RATE_BASE,global_step,mnist.train.num_examples/BATCH_SIZE,LEARNING_RATE_DECAY)
 
+    #使用优化算法
+    train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss,global_step=global_step)
+
+    #一次完成多个操作，通过反向传播来更新神经网络中的参数，又更新每一个参数的滑动平均值
+    with tf.control_dependencies([train_step,variable_averages_op]):
+        train_op = tf.no_op(name='train')
+
+    correct_prediction = tf.equal(tf.argmax(average_y,1),tf.argmax(y_,1))
+    #下面的运算首先将bool值转换为实数型，然后计算平均值。这个平均值就是模型在这一组数据上的正确率。
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32));
+
+    #初始化回话并开始训练
+    with tf.Session() as sess:
+        tf.initialize_all_variables().run()
+        #准备验证数据
+        validata_feed = {x: mnist.validation.images,y_: mnist.validation.labels}
+
+        #测试数据
+        test_feed = {x:mnist.test.images,y_:mnist.test.labels}
+
+        #迭代神经网络
+        starttime = datetime.datetime.now()
+        for i in range(TRAINGING_STEPS):
+            #每一千轮输出数据
+            if(i%1000==0):
+                validate_acc = sess.run(accuracy,feed_dict=validata_feed)
+                print("After %d traning step(s),validation accuracy""using average model is %g" % (i,validate_acc))
+
+            #产生这一轮使用的batch数据集
+            xs,ys = mnist.train.next_batch(BATCH_SIZE)
+            sess.run(train_op,feed_dict={x:xs,y_:ys})
+
+        #训练结束，在测试数据检测最终正确率
+        test_acc = sess.run(accuracy,feed_dict=test_feed)
+        print(TRAINGING_STEPS,test_acc)
+
+        endtime = datetime.datetime.now()
+        print("总耗时 %g" % (endtime - starttime).seconds +" 秒")
+
+#主程序入口
+def main(argv=None):
+    mnist = input_data.read_data_sets("D:/mnist_dataset/",one_hot=True)
+    train(mnist)
+
+#tensorflow提供主程序入口，如下
+if __name__ == '__main__':
+    tf.app.run()
